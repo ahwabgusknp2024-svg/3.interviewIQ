b2c44b9891583cadc9e20091bb09bb22ae60706d
ahwabgusknp2024-svg <ahwab.gusknp2024@gmail.com>
2026-07-26 11:11:56 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "https://three-interviewiq-1-server-latest.onrender.com"
+export const ServerUrl  = "https://three-interviewiq-1-server-latest-25u2.onrender.com"
 
 function App() {
 
